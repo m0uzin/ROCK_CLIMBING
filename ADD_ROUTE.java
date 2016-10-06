49a91fe46243482af94670edc9e96db668933e5f
Mr.Robot <bodapeter8@gmail.com>
2016-10-06 15:19:45 +0300

    Add files via upload

---
+++ ADD_ROUTE.java
@@ -0,0 +1,13 @@
+package a0.add_routes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ADD_ROUTE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__route);
+    }
+}
